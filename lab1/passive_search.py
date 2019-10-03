cbb342f837ee80718b75301562aa5b7ae6bc11e4
kyplyk <avdosev1@yandex.ru>
2019-10-03 17:45:16 +0400

    add test

--- lab1/passive_search.py
+++ lab1/passive_search.py
@@ -1,10 +1,4 @@
 import numpy as np
-import math
-
-def func_1(x):
-    return 4*x**3 - 8*x**2 - 11*x + 5
-
-
 
 def passive_search(a: float, b: float, N: int, func) -> float:
     step = (b-a)/N
@@ -15,21 +9,3 @@ def passive_search(a: float, b: float, N: int, func) -> float:
     min_index = value_points.argmin()
     
     return x_points[min_index]
-
-def main():
-    func_number = int(input("Type number of function: "))
-
-    try:
-        arr_of_func = [func_1]
-        func = arr_of_func[func_number-1]
-    except Exception as identifier:
-        print("Function not found")
-        return
-    
-
-    min_index = passive_search(-1, 2, 10, func)
-    min_value = func(min_index)
-
-    print(f"x={min_index} y={min_value}")
-
-main()
