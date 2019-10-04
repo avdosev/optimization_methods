1a1722f91f2520df40545022fb5c2d8ff161809f
kyplyk <avdosev1@yandex.ru>
2019-10-04 13:09:44 +0400

    упростил функцию

--- one-dimensional_optimization/method_devisa_svenna_campy.py
+++ one-dimensional_optimization/method_devisa_svenna_campy.py
@@ -2,38 +2,34 @@ from typing import Callable, Tuple
 
 def search_local_min(func: Callable[[float], float], x0: float, h: float = 0.0001) -> Tuple[float, float]:
     f0 = func(x0)
-    f_step = func(x0+h)
-    if f0 > f_step:
+    if f0 > func(x0+h):
         a = x0
-        x_next = x0 + h
     elif func(x0-h) >= f0:
         a = x0 - h
         b = x0 + h
         return (a,b)
     else:
         b = x0
-        x_next = x0 - h
         h = -h
 
     
     def xk(k: int):
         return x0 + (2**(k-1))*h
 
+    def assign_if(is_a, value):
+        if is_a:
+            a = value
+        else:
+            b = value
+
     k = 2
     while True:
         xk0, xk1 = xk(k), xk(k-1)
         if func(xk0) >= func(xk1):
-            if h > 0:
-                b = xk0
-            else:
-                a = xk0
+            assign_if(h < 0, xk0)
             break
         else:
-            if h > 0:
-                a = xk1
-            else:
-                b = xk1
-
+            assign_if(h > 0, xk1)
         k += 1
 
     return (a,b)
