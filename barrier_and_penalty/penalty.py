25bb1924ebdb7bd4ff6d4ed75ae2843add703a50
kyplyk <avdosev1@yandex.ru>
2019-11-29 21:39:29 +0400

    fix autism

--- barrier_and_penalty/penalty.py
+++ barrier_and_penalty/penalty.py
@@ -5,18 +5,17 @@ from optimal_gradient_method import *
 def getAuxilitaryFunctionResult(f, r, rest_eq, rest_not_eq, x):
     x1 = x[0]
     x2 = x[1]
-    H = sum([pow(abs(i(x1,x2)),2) for i in rest_eq])
-    H2 = sum([pow(max(0, i(x1,x2)),2) for i in rest_not_eq])
-
-    return f(x) + r*(H+H2)
+    H = 0
+    for i in rest_eq:
+        H += pow(abs(i(x1,x2)),2)
+    for i in rest_not_eq:
+        H += pow(max(0, i(x1,x2)),2)
+    return f(x) + r*H
 
 def penalty(x0, f, r, z, eps, rest_eq, rest_not_eq):
     xcur = np.array(x0)
-    auxilitary = lambda x: getAuxilitaryFunctionResult(f, r, rest_eq, rest_not_eq, x)
-    xnew = optimal_gradient_method( auxilitary, xcur, eps )
-
-    while ((xcur-xnew)**2).sum() > eps:
+    xnew = optimal_gradient_method(lambda x:getAuxilitaryFunctionResult(f, r, rest_eq, rest_not_eq, x), xcur)
+    while ((xcur - xnew)**2).sum()>eps:
         r *= z
-        xcur, xnew = xnew, optimal_gradient_method( auxilitary, xcur, eps )
-
+        xcur, xnew = xnew, optimal_gradient_method(lambda x:getAuxilitaryFunctionResult(f, r, rest_eq, rest_not_eq, x), xcur)
     return xnew
--- barrier_and_penalty/test.py
+++ barrier_and_penalty/test.py
@@ -1,71 +1,136 @@
-def f1(x):
+import barrier
+import penalty
+
+def f(x):
     x1,x2 = x
     return x1**2+x2**2
 
+def f1(x):
+    x1,x2 = x
+    return 4*x1**2+8*x1-x2-3
+
+def f2(x):
+    x1,x2 = x
+    return 4/x1+9/x2+x1+x2
+
 restrictionsOfEquality = [
+    [lambda x1,x2: x1+x2+2],
     [lambda x1,x2: x1+x2-2],
-    [],
     [lambda x1,x2: x1-1],
-    [lambda x1,x2: x1-1],
-    [],
-    [],
     []
 ]
 
 restrictionsOfNotEquality = [
-    [],
-    [lambda x1,x2: -x1+1,lambda x1,x2: x1+x2-2],
+    [lambda x1,x2: x1+x2-6,lambda x1,x2: -x1,lambda x1,x2: -x2],
+    [lambda x1,x2: x1+x2-4,lambda x1,x2: -x1,lambda x1,x2: -x2],
     [lambda x1,x2: x1+x2-2],
-    [lambda x1,x2: x1+x2-2],
     [lambda x1,x2: -x1+1,lambda x1,x2: x1+x2-2],
-    [lambda x1,x2: -x1+1,lambda x1,x2: x1+x2-2],
-    [lambda x1,x2: -x1+1,lambda x1,x2: x1+x2-2]
+    []
 ]
 
-startPoints = [
-    [0., 0.],
-    [2., -1.],
-    [0., 1.],
-    [2., 2.],
-    [0., 1.],
-    [2., 2.],
-    [1., 1.]
-]
+while(True):
+    print("""Выберете функцию которую нужно минимизировать:
+    1 - 1.1
+    2 - 1.2
+    3 - 1.3
+    4 - 2.5
+    5 - 2.6
+    """)
+    chosenFoon = int(input())
 
-funcsToTest = [
-    f1,
-    f1,
-    f1,
-    f1,
-    f1,
-    f1,
-    f1
-]
-
-from barrier import *
-from penalty import *
-
-print("PENALTY METHOD: ")
-for i in range(6):
-    res = penalty(startPoints[i], 
-        funcsToTest[i], 
+    if chosenFoon == 1:
+        print("\nМинимизируемая функция: x1^2+x2^2")
+        print("Метод штрафных функций: ")
+        res = penalty.penalty([0.,0.], 
+        f, 
         0.1, 
         10, 
         0.01,
-        restrictionsOfEquality[i], 
-        restrictionsOfNotEquality[i]
-    )
-    r = "".join([f"{j:.{5}f} " for j in res])
-    print(f"{i}:", r) 
+        restrictionsOfEquality[1], 
+        []) 
+        r = ""
+        for j in res:
+            r+=f"{j:.{5}f} "
+        print(r) 
+        print (res[0]**2+res[1]**2)
 
-print("BARRIER METHOD: ")
-for i in [1]:
-    res = barrier(startPoints[i], 
-        funcsToTest[i], 
+    elif chosenFoon == 2:
+        print("\nМинимизируемая функция: x1^2+x2^2")
+        print("Метод штрафных функций: ")
+        res = penalty.penalty([0.,1.], 
+        f, 
+        0.1, 
+        10, 
+        0.01,
+        restrictionsOfEquality[2], 
+        restrictionsOfNotEquality[2]) 
+        r = ""
+        for j in res:
+            r+=f"{j:.{5}f} "
+        print(r) 
+        print (res[0]**2+res[1]**2)
+
+    elif chosenFoon == 3:
+        print("\nМинимизируемая функция: x1^2+x2^2")
+        print("Метод барьерных функций: ")
+        res = penalty.penalty([2.,2.], 
+        f, 
+        0.1, 
+        10, 
+        0.01,
+        [], 
+        restrictionsOfNotEquality[3]) 
+        r = ""
+        for j in res:
+            r+=f"{j:.{5}f} "
+        print(r) 
+        print (res[0]**2+res[1]**2)
+
+    elif chosenFoon == 4:
+        print("\nМинимизируемая функция: 4*x1^2+8*x1-x2-3")
+        print("Метод барьерных функций: ")
+        res = barrier.barrier([-1.,-7.], 
+        f1, 
         100, 
         0.5, 
-        0.00625,
-        restrictionsOfNotEquality[i]
-    )
-    r = "".join([f"{j:.{5}f} " for j in res])
-    print(f"{i}:", r) 
+        0.005,
+        restrictionsOfEquality[0])
+        r =""
+        for j in res:
+            r+=f"{j:.{5}f} "
+        print(r)
+        print (4*res[0]**2+8*res[0]-res[1]-3)
+
+    elif chosenFoon == 5:
+        print("\nМинимизируемая функция: 4/x1+9/x2+x1+x2")
+        print("Метод штрафных функций при ограничении (а): ")
+        res = penalty.penalty([4.,5.], 
+        f2, 
+        0.1, 
+        10, 
+        0.01,
+        [], 
+        restrictionsOfNotEquality[0]) 
+        r = ""
+        for j in res:
+            r+=f"{j:.{5}f} "
+        print(r)
+        print (4/res[0]+9/res[1]+res[0]+res[1])
+
+        print("Метод штрафных функций при ограничении (б): ")
+        res = penalty.penalty([2.,3.], 
+        f2, 
+        0.1, 
+        10, 
+        0.1,
+        [], 
+        restrictionsOfNotEquality[1]) 
+        r = ""
+        for j in res:
+            r+=f"{j:.{5}f} "
+        print(r)
+        print (4/res[0]+9/res[1]+res[0]+res[1])
+
+
+    
+
